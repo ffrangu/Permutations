e587a477cf7ff68ca7f79fcb70e8726665a8c436
ffrangu <fitonfrangi@gmail.com>
2021-10-25 21:44:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintArray(
+                Permute(new int[] { 1, 2, 3 })
+            );
+
+        }
+
+        static IList<IList<int>> Permute(int[] nums)
+        {
+            var list = new List<IList<int>>();
+            return DoPermute(nums, 0, nums.Length - 1, list);
+        }
+
+        static IList<IList<int>> DoPermute(int[] nums, int start, int end, IList<IList<int>> list)
+        {
+            if (start == end)
+            {
+                list.Add(new List<int>(nums));
+            }
+            else
+            {
+                for (var i = start; i <= end; i++)
+                {
+                    Swap(ref nums[start], ref nums[i]);
+                    DoPermute(nums, start + 1, end, list);
+                    Swap(ref nums[start], ref nums[i]);
+                }
+            }
+
+            return list;
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void PrintArray(IList<IList<int>> lists)
+        {
+            Console.WriteLine("[");
+            foreach (var list in lists)
+            {
+                Console.WriteLine($"[{string.Join(",", list)}]");
+            }
+            Console.WriteLine("]");
+
+            Console.ReadLine();
+        }
+    }
+}
